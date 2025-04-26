543ad6926f3b30b878f02fbbbd0ec7c8f3697c49
Muhammad <muhvmmvdessvm@gmail.com>
2025-04-26 14:25:14 +0300

    Update Footer.jsx

--- src/components/Main/Footer.jsx
+++ src/components/Main/Footer.jsx
@@ -10,24 +10,24 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-gray-400 text-[11.5px] sm:text-[12.5px] py-3 px-4 sm:px-15">
+    <footer className="bg-black text-gray-400 text-[12px] sm:text-[12.5px] py-3 px-4 sm:px-15">
       <div className="flex flex-col sm:flex-row gap-2 justify-between items-center">
         {/* Social Links */}
         <div className="flex gap-5 order-1 sm:order-2">
           <a href="https://linkedin.com/in/muhmedv" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faLinkedinIn} className="hover:text-white text-[13px] sm:text-[15px] transition" />
+            <FontAwesomeIcon icon={faLinkedinIn} className="hover:text-white text-[14px] sm:text-[15px] transition" />
           </a>
           <a href="https://github.com/lil-de7k" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faGithub} className="hover:text-white text-[12.5px] sm:text-[15.5px] transition" />
+            <FontAwesomeIcon icon={faGithub} className="hover:text-white text-[13.5px] sm:text-[15.5px] transition" />
           </a>
           <a href="https://instagram.com/lil__de7k" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faInstagram} className="hover:text-white text-[13px] sm:text-[16px] transition" />
+            <FontAwesomeIcon icon={faInstagram} className="hover:text-white text-[14px] sm:text-[16px] transition" />
           </a>
           <a href="https://wa.me/201012857997" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faWhatsapp} className="hover:text-white text-[13.5px] sm:text-[17px] transition" />
+            <FontAwesomeIcon icon={faWhatsapp} className="hover:text-white text-[14.5px] sm:text-[17px] transition" />
           </a>
           <a href="mailto:muhvmmvdessvm@gmail.com">
-            <FontAwesomeIcon icon={faEnvelope} className="hover:text-white text-[12px] sm:text-[15px] transition" />
+            <FontAwesomeIcon icon={faEnvelope} className="hover:text-white text-[13px] sm:text-[15px] transition" />
           </a>
         </div>
 
